0b23e9009716f3e9e43867cd1e366d856cab7c90
Nick <kit1datasvit@gmail.com>
2025-08-15 09:42:22 +0300

    - fix bottombar

--- app/src/main/java/com/runner/cat/MainActivity.kt
+++ app/src/main/java/com/runner/cat/MainActivity.kt
@@ -2,14 +2,10 @@ package com.runner.cat
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
 import com.runner.cat.navigation.Navigation
 import com.runner.cat.ui.theme.MyApplicationTheme
@@ -18,12 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Включение edge-to-edge режима
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             MyApplicationTheme {
-                // Удалите ProvideWindowInsets, используйте WindowInsets напрямую
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
